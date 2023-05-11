4a8a3b12706282475ff222db592dc975224516a3
Michael Hofweller <michael.hofweller@gmail.com>
2023-05-11 14:51:27 -0700

    smart contract updated to recieve payment, set records and launch dns with info in the constructor

--- scripts/run.js
+++ scripts/run.js
@@ -3,22 +3,24 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
-  const domainContract = await domainContractFactory.deploy();
+  const domainContract = await domainContractFactory.deploy('labs');
   await domainContract.deployed();
+
   console.log("Contract deployed to: ", domainContract.address);
   console.log("Contract deployed by: ", owner.address);
 
-  let txn = await domainContract.register("scale3.labs");
+  const userInput = "scale3";
+
+  let txn = await domainContract.register(userInput, {
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
   await txn.wait();
 
-  const domainAddress = await domainContract.getAddress("scale3.labs");
-  console.log("Owner of domain: ", domainAddress);
+  const address = await domainContract.getAddress(userInput);
+  console.log("Owner of domain :", address);
 
-  // Trying to set a record that doesn't belong to me!
-  txn = await domainContract
-    .connect(randomPerson)
-    .setRecord("doom", "Haha my domain now!");
-  await txn.wait();
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 };
 
 const runMain = async () => {
